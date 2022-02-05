bfa94b0098f77c42a7c789abad7977120a03abc9
Daniel Interiano <dannyboyint@gmail.com>
2022-02-05 13:24:53 -0500

    Modified more files and deleted others that were throwing errors in the console.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Home() {
     return (
         <div className="hero">
-            <div class="card bg-dark text-white border-0">
-                <img src="/assets/bg.jpg" class="card-img" alt="Background" height="500px"/>
-                <div class="card-img-overlay d-flex flex-column ">
+            <div className="card bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" className="card-img" alt="Background" height="500px"/>
+                <div className="card-img-overlay d-flex flex-column ">
                     <div className="container">
-                    <h5 class="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
-                    <p class="card-text lead fs-2"> 
-                        Check out all the trends, if you want to be the hokage
+                    <h5 className="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
+                    <p className="card-text lead fs-2"> 
+                        Check out all the trends, if you want to be cool
                     </p>
                     </div>
                 </div> 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,13 +9,16 @@ export default function Products() {
     useEffect(() => {
         const getProducts = async () => {
             setLoading(true);
-            const response = await fetch("https://fakestoreapi.com.products");
+            const response = await fetch("https://fakestoreapi.com/products");
             if(componentMounted){
                 setData(await response.clone().json());
                 setFilter(await response.json());
                 setLoading(false);
+                console.log(filter)
             }
-            return () =>
+            return () => {
+                componentMounted = false;
+            }
         }
       getProducts();
     
