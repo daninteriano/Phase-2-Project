21f0c1fcf56b1e8b669667e9b31880e2bc37018b
Daniel Interiano <dannyboyint@gmail.com>
2022-02-08 02:50:23 -0500

    Continuing modifying the contant us page, and trying to shorten the length of my input fields.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export default function Contact() {
-  return <div>
-      <h1>Here is my contact page!</h1>
-  </div>;
+    return (
+    <div>
+        <div class="mb-3">
+            <h2>Contant Us</h2>
+            <h4>We want to hear from you!</h4>
+            <label for="fullName" class="form-label">Full Name</label>
+            <input type="text" class="form-control" id="fullNameForm" placeholder="FirstAnd LastName" />
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="emailFormControlInput" placeholder="name@example.com" />
+        </div>
+        <div class="mb-3 " width="50px">
+            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" ></textarea>
+        </div>
+    </div>
+    )
 }
