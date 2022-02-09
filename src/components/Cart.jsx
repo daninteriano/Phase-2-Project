4aebcbd379e20f5d8b559f6b2a3174b74c07e12d
Daniel Interiano <dannyboyint@gmail.com>
2022-02-09 15:13:17 -0500

    Adding clear cart and pay now buttons to the cart and adding finishing touches

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,9 +3,10 @@ import { useCart } from "react-use-cart";
 
 
 export default function Cart() {
-  const { isEmpty, totalUniqueItems, items, totalItems, 
-    cartTotal, updateItemQuantity, removeItem, emptyCart} = useCart();
-    if(isEmpty)return <h1 className='text-center'>Your Cart is Empty</h1>
+    const { isEmpty, totalUniqueItems, items, totalItems,
+        cartTotal, updateItemQuantity, removeItem, emptyCart } = useCart();
+
+    if (isEmpty) return <h1 className='text-center'>Your Cart is Empty</h1>
 
     return (
         <section className='py-4 container'>
@@ -13,21 +14,42 @@ export default function Cart() {
                 <div className='col-12'>
                     <h5>Cart ({totalUniqueItems}) total Items: ({totalItems})</h5>
                     <table className='table table-light table-hover m-0'>
-                        {items.map((thing, index) => {
-                            <tr key={index}>
-                                <td>
-                                    <img src={thing.img} style={{height: "6rem"}}/>
-                                </td>
-                                <td>{thing.title}</td>
-                                <td>{thing.price}</td>
-                                <td>Quantity: ({thing.quantity})</td>
-                                
-                            </tr>
-                        })}
+                        <tbody>
+                            {/* building out the card for the cart using table tag*/}
+                            {items.map((thing, index) => {
+                                return (
+                                <tr key={index}>
+                                    <td>
+                                        <img src={thing.image} style={{ height: "6rem" }} />
+                                    </td>
+                                    <td>{thing.title}</td>
+                                    <td>{thing.price}</td>
+                                    <td>Quantity: ({thing.quantity})</td>
+                                    <td>
+                                        {/* adding the quantitiy functions to the buttons */}
+                                        <button onClick={() => updateItemQuantity(thing.id, thing.quantity - 1)}
+                                        className='btn btn-danger ms-2'>-</button>
+                                        <button onClick={() => updateItemQuantity(thing.id, thing.quantity + 1)}
+                                        className='btn btn-success ms-2'>+</button>
+                                        <button onClick={() => removeItem(thing.id)}
+                                        className='btn ms-2'>Remove Item</button>
+                                    </td>
+                                </tr>)
+                            })}
+                        </tbody>
                     </table>
+                    <div className='col-auto ms-auto'>
+                        <h2>Total Price:$ {cartTotal}</h2>
+                    </div>
+                    <div>
+                        <button onClick={() => emptyCart()} className='btn btn-danger m-2'>
+                            Clear Cart 
+                        </button>
+                        <button className='btn btn-primary m-2'>Pay Now</button>
+                    </div>
                 </div>
             </div>
         </section>
-      
-  )
+
+    )
 }
