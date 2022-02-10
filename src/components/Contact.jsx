aa5042d5508a8c8ac8763196f4e2e8318dbc228c
Daniel Interiano <dannyboyint@gmail.com>
2022-02-10 14:16:24 -0500

    Further customization with CSS as well as updating form:

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 export default function Contact() {
     const [formData, setFormData] = useState({
         name: "",
@@ -9,7 +10,7 @@ export default function Contact() {
 
     function handleChange(event) {
         setFormData({
-            ...formData, [event.target.name]: event.target.value,
+            ...formData, [event.target.name]: event.target.value
         });
     }
     function handleSubmit(event) {
@@ -31,28 +32,34 @@ export default function Contact() {
 
     return (
         <div className='container '>
-            <form onSubmit={handleSubmit} className="container d-flex justify-content-center rounded bg-info border border-info h-100 w-100 my-5 " >
+            <form onSubmit={handleSubmit} className="container d-flex justify-content-center rounded bg-warning border border-warning h-100 w-100 my-5 p-5" >
                 <h2>Contact Us</h2>
                 <div class="mb-3 px-4">
                     <label for="nameFormControlInput" class="form-label pt-2">
                         Full Name
                     </label>
-                    <input type="text" class="form-control" placeholder="Full Name" onChange={handleChange} />
+                    <input type="text" class="form-control" placeholder="Full Name" value={formData.name} 
+                    onChange={(e) => setFormData({...formData, [e.target.name]: e.target.value})} />
                     <label for="emailFormControlInput" class="form-label pt-2">
                         Email address
                     </label>
-                    <input type="email" class="form-control" placeholder="name@example.com" onChange={handleChange} />
+                    <input type="email" value={formData.email} class="form-control" placeholder="name@example.com" onChange={(e) => setFormData(e.target.value)} />
                 </div>
                 <div class="mb-3 px-4 w-100">
                     <label for="textFormControlInput" class="form-label">
                         Message
                     </label>
-                    <textarea class="form-control " rows="3" onChange={handleChange} ></textarea>
+                    <textarea class="form-control " rows="3" value={formData.message} onChange={(e) => setFormData(e.target.value)} ></textarea>
                     <button type="submit" class="btn btn-primary px-3 mt-3">
                         Submit
                     </button>
                 </div>
             </form>
+            <div className='container d-flex flex-wrap bg-warning'>
+                <i class="fa fa-envelope m-4"> daninteriano@yahoo.com</i>
+                <i class="fa fa-phone m-4"> 212-342-0001</i>
+                <i class="fa fa-location-smile m-4"> * Brooklyn, New York</i>
+            </div>
         </div>
     )
 }
