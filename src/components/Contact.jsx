8844421c5016770794484b82bd4e8ce89abe09bd
Daniel Interiano <dannyboyint@gmail.com>
2022-02-10 19:19:50 -0500

    Finished the project! Finally got my post request to work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,54 +2,51 @@ import React, { useState } from 'react';
 
 
 export default function Contact() {
+
     const [formData, setFormData] = useState({
         name: "",
         email: "",
-        message: ""
+        message: "",
     });
-
     function handleChange(event) {
         setFormData({
             ...formData, [event.target.name]: event.target.value
         });
     }
+    
     function handleSubmit(event) {
         event.preventDefault();
+        console.log(formData)
         fetch("http://localhost:3000/messages", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Accept": "application/json"
             },
-            body: {
-                "name": formData.name,
-                "email": formData.email,
-                "message": formData.message
-            }
+            body: JSON.stringify(formData),
         })
             .then((res) => res.json())
-            .then((data) => console.log(data))
+            .then((data) => setFormData(data))
     }
 
     return (
         <div className='container '>
             <form onSubmit={handleSubmit} className="container d-flex justify-content-center rounded bg-warning border border-warning h-100 w-100 my-5 p-5" >
-                <h2>Contact Us</h2>
                 <div class="mb-3 px-4">
+                <h2>Contact Us</h2>
+                <hr />
                     <label for="nameFormControlInput" class="form-label pt-2">
-                        Full Name
-                    </label>
-                    <input type="text" class="form-control" placeholder="Full Name" value={formData.name} 
-                    onChange={(e) => setFormData({...formData, [e.target.name]: e.target.value})} />
+                        Full Name </label>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} class="form-control" placeholder="Full Name" />
                     <label for="emailFormControlInput" class="form-label pt-2">
-                        Email address
-                    </label>
-                    <input type="email" value={formData.email} class="form-control" placeholder="name@example.com" onChange={(e) => setFormData(e.target.value)} />
+                        Email address</label>
+                    <input type="email" name="email" value={formData.email}  onChange={handleChange} class="form-control" placeholder="name@example.com" />
                 </div>
                 <div class="mb-3 px-4 w-100">
                     <label for="textFormControlInput" class="form-label">
                         Message
                     </label>
-                    <textarea class="form-control " rows="3" value={formData.message} onChange={(e) => setFormData(e.target.value)} ></textarea>
+                    <textarea class="form-control " name="message" rows="3" value={formData.message} onChange={handleChange} ></textarea>
                     <button type="submit" class="btn btn-primary px-3 mt-3">
                         Submit
                     </button>
