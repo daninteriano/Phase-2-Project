dfbbef79e597ece591f52db498189d86f7018c2b
Daniel Interiano <dannyboyint@gmail.com>
2022-02-10 22:57:08 -0500

    Continuing to try and align items in the products list

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -37,7 +37,7 @@ export default function Products() {
 
     const ShowProducts = () => {
         return (
-            <div className='container bg-warning bg-opacity-50 rounded border border-warning '> 
+            <div className='container bg-warning bg-opacity-50 rounded border border-warning border-4'> 
                 {/* here we have the different filter options for the products */}
                 <div className="buttons d-flex flex-row justify-content-center mb-5 mt-5 pb-5">
                     <button className="btn btn-outline-dark me-2" onClick={() => setFilter(data)}>All</button>
@@ -45,12 +45,15 @@ export default function Products() {
                     <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
                     <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("jewelery")}>Jewelery</button>
                     <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("electronics")}>Electronic</button>
+                
                 </div>
+                <div className='container '>
                 {filter.map((product) => {
                     return (
-                            <div className="card h-100 text-center p-4 bg-warning" key={product.id}>
+                        
+                            <div className="card w-25 h-15 text-center p-4 m1-0 mx-0 my-0 bg-warning border-3" key={product.id}>
                                 <img src={product.image} className="card-img-top " alt={product.title} height="250px"/>
-                                    <div className="card-body bg-warning bg-opacity-50">
+                                    <div className="card-body">
                                         <h5 className="card-title mb-0">{product.title.substring(0,12)}...</h5>
                                         <p className="card-text lead fw-bold">
                                             ${product.price}
@@ -59,8 +62,10 @@ export default function Products() {
                                         <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Buy Now</NavLink>
                                     </div>
                             </div>
+                        
                     )
                 })}
+                </div>
             </div>
         )
     }
